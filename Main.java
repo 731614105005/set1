c5798263e4ae7fa5645e161a10d44b097af74b35
731614105005 <bawadharani96@gmail.com>
2017-07-04 17:40:57 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+# set1
+import java.io.*;
+import java.util.*;
+public class Main{
+  
+  public static void main(String args[])throws IOException{
+    
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the number");
+    int b=sc.nextInt();
+    if(b>0)
+    {
+           System.out.println("b is positive");
+           }
+           else if(b<0){
+                       System.out.println("b is negative");
+           } 
+           else {
+                    System.out.println("b is zero");
+           }
+           }
+           }
